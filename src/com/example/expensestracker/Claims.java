185c4240f35429b55532d2255fa43533b95d7a9c
mrthegreensky <azhong@ualberta.ca>
2015-01-20 02:02:55 -0700

    Added statuses for claims class

--- src/com/example/expensestracker/Claims.java
+++ src/com/example/expensestracker/Claims.java
@@ -7,6 +7,11 @@ public class Claims {
 	
 	protected List<Expense> list = new ArrayList<Expense>();
 	
+	private int status = 0; // where status = 0 not submitted and is the default status
+						// status = 1 is submitted but still being reviewed
+						// status = 2 is rejected/returned and needs improvements to be done
+						// status = 3 is accepted no further edits allowed
+	
 	public void addClaim(Expense expense) {
 		list.add(expense);
 	}
@@ -21,6 +26,22 @@ public class Claims {
 	
 	public List<Expense> getClaims() {
 		return list;
+	}
+	
+	public int getStatus() {
+		return this.status;
+	}
+	
+	public void setSubmit() {
+		this.status = 1;
+	}
+	
+	public void setReject() {
+		this.status = 2;
+	}
+	
+	public void setAccept() {
+		this.status = 3;
 	}
 
 }
